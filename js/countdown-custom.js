f18c616915aa868a56b951ad39e71d7b77751279
sreelakshmi2708 <84312012+sreelakshmi2708@users.noreply.github.com>
2023-06-11 22:45:37 +0530

    Update countdown

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 5, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 5, 24, 8)}); // year, month, date, hour
         });
 });		
 
