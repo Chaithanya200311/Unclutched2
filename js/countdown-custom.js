09c923253c9ebb8c64940503d70ed3b4928fbd41
sreelakshmi2708 <sreelakshmi2708@gmail.com>
2023-06-12 00:09:16 +0530

    countdown

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 5, 24, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 5, 24, 0)}); // year, month, date, hour
         });
 });		
 
